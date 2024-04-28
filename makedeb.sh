f50c20e3bebdf3ae316acea9757cb2562902bcf9
ryanpdx <rmedick@protonmail.com>
2024-04-28 11:42:51 -0700

    fix debain package configs

--- makedeb.sh
+++ makedeb.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+PACKAGE=prucam-pirt1280
 DATE=`date "+%a, %d %b %Y %T %z"`
 VERSION=`git describe --tags --abbrev=0`
 VERSION="${VERSION:1}"  # remove leading 'v'
 
 cat > "debian/changelog" <<-__EOF__
-prucam ($VERSION-0) buster; urgency=low
+$PACKAGE ($VERSION-0) buster; urgency=low
 
   * See git logs at https://github.com/oresat/oresat-linux-prucam/releases
 
@@ -13,3 +14,6 @@ prucam ($VERSION-0) buster; urgency=low
 __EOF__
 
 dpkg-buildpackage -us -uc
+
+mv ../$PACKAGE*.deb .
+rm -f ../$PACKAGE*
